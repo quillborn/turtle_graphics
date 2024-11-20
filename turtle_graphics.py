3e5b67b7beeec8dc43e186e750c4637f9942f651
Carter Terry Jr <cptj@kaavik.com>
2024-11-19 21:09:05 -0500

    V 0.1 experiments with turtle module

---
+++ turtle_graphics.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+dribble = Turtle()
+dribble.shape("turtle")
+dribble.color("green")
+dribble.forward(100)
+dribble.right(90)
+
+
+screen = Screen()
+screen.exitonclick()
