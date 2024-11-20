5dda24521cb77d0d5b7610702a1bec073f575996
Carter Terry Jr <cptj@kaavik.com>
2024-11-19 22:11:14 -0500

    V 0.1.2 defined random_walk function

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -1,10 +1,37 @@
 from turtle import Turtle, Screen
+import random
+
+screen = Screen()
 dribble = Turtle()
 dribble.shape("turtle")
 dribble.color("green")
-dribble.forward(100)
-dribble.right(90)
 
+def random_walk():
+    """Turtle will paint a random walk path."""
+    directions = [0,90,180,270]
+    colors = ["red", "blue","green","yellow","orange","purple","pink","brown"]
+    dribble.pensize(20)
+    dribble.speed(10)
 
-screen = Screen()
-screen.exitonclick()
+    counter = 0
+    while True:
+        x = random.choice(directions)
+        y = random.choice(colors)
+        dribble.color(y)
+        dribble.right(x)
+        print(x)
+        dribble.forward(50)
+        counter += 1
+        if counter == 50:
+            while True:
+                q = input("continue? Y/N")
+                if q == "y":
+                    counter = 0
+                    break
+                elif q == "n":
+                    screen.exitonclick()
+                    exit()
+                else:
+                    continue
+
+random_walk()
